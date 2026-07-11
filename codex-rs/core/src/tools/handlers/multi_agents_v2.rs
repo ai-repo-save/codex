eab7d04bf0e300e1f69919771e93523e519ac5b4
bluebird <linlanxi7552659@gmail.com>
2026-07-11 20:05:40 +0800

    Limit parent request test exports

--- codex-rs/core/src/tools/handlers/multi_agents_v2.rs
+++ codex-rs/core/src/tools/handlers/multi_agents_v2.rs
@@ -26,7 +26,9 @@ use serde::Deserialize;
 use serde::Serialize;
 use serde_json::Value as JsonValue;
 
+#[cfg(test)]
 pub(crate) use ask_parent::AskParentResult;
+#[cfg(test)]
 pub(crate) use ask_parent::AskParentStatus;
 pub(crate) use ask_parent::Handler as AskParentHandler;
 pub(crate) use followup_task::Handler as FollowupTaskHandler;
